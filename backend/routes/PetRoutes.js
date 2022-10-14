da5ab292004e82936a33ad6aa0be3c2729a21304
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2022-10-14 11:54:23 -0300

    resgatnado um pet pelo ID

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -9,6 +9,7 @@ const {imageUpload} = require('../helpers/image-upload')
 router.post('/create', verifytoken, imageUpload.array('images'), PetController.create)
 router.get('/mypets', verifytoken, PetController.getAllUserPets)
 router.get('/myadoptions', verifytoken, PetController.getAllUserAdoptions)
+router.get('/:id', PetController.getPetById)
 router.get('/', PetController.getAll)
 
 
