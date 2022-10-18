d0641fc8e9cb5afaabe906d07cd8c9c58cb5b51b
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2022-10-18 18:41:30 -0300

    css da Navbar e do Footer (de forma modular)

--- frontend/src/components/layouts/Navbar.js
+++ frontend/src/components/layouts/Navbar.js
@@ -2,11 +2,12 @@ import {Link} from 'react-router-dom'
 
 import Logo from '../../assets/img/logo.png'
 
+import styles from './Navbar.module.css'
+
 function Navbar(){
     return(
-
-        <nav>
-            <div>
+        <nav className={styles.navbar}>
+            <div className={styles.navbar_logo}>
                 <img src={Logo} alt="Adote" />
                 <h2>Adote</h2>
             </div>
