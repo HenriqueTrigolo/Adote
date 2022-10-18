e018d3deeb59a60293413b9b15da46c38c3ce7cd
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2022-10-18 10:17:49 -0300

    finalização do CRUD de Pets

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -128,13 +128,74 @@ module.exports = class PetController{
         const user = await getUserByToken(token)
 
         if(pet.user._id.toString() !== user._id.toString()){
-            res.status(404).json({message:'Ocorreu um erro, tente novamente mais tarde!'})
+            res.status(422).json({message:'Ocorreu um erro, tente novamente mais tarde!'})
             return
         }
 
         await Pet.findByIdAndDelete(id)
 
         res.status(200).json({message: 'Pet Removido com sucesso!'})
+    }
+
+    static async updatePet(req, res){
+        const id = req.params.id
+
+        const {name, age, weight, color, available} = req.body
+
+        const images = req.files
+
+        const updateData = {}
+
+        //check if pet exist
+        const pet = await Pet.findOne({_id: id})
+
+        if(!pet){
+            res.status(404).json({message:'Pet não encontrado'})
+            return
+        }
+
+        //check if lodded user registered the pet
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if(pet.user._id.toString() !== user._id.toString()){
+            res.status(404).json({message:'Ocorreu um erro, tente novamente mais tarde!'})
+            return
+        }
+
+        //validations
+        if(!name){
+            return res.status(422).json({message:'O nome é obrigatório!'})
+        }else{
+            updateData.name = name
+        }
+        if(!age){
+            return res.status(422).json({message:'A idade é obrigatória!'})
+        }else{
+            updateData.age = age
+        }
+        if(!weight){
+            return res.status(422).json({message:'O peso é obrigatório!'})
+        }else{
+            updateData.weight = weight
+        }
+        if(!color){
+            return res.status(422).json({message:'A cor é obrigatória!'})
+        }else{
+            updateData.color = color
+        }
+        if(images.length === 0){
+            return res.status(422).json({message:'A imagem é obrigatória!'})
+        }else{
+            updateData.images = []
+            images.map((image) => {
+                updateData.images.push(image.filename)
+            })
+        }
+
+        await Pet.findByIdAndUpdate(id, updateData)
+
+        res.status(200).json({message: "Pet atualizado com sucesso!"})
     }
 
 }
--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -11,6 +11,7 @@ router.get('/mypets', verifytoken, PetController.getAllUserPets)
 router.get('/myadoptions', verifytoken, PetController.getAllUserAdoptions)
 router.get('/:id', PetController.getPetById)
 router.delete('/:id', verifytoken, PetController.deletePetById)
+router.patch('/:id', verifytoken, imageUpload.array('images'), PetController.updatePet)
 router.get('/', PetController.getAll)
 
 
