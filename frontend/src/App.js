c7140a655ac4537fa3642dd1d7b14a3536145006
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2022-10-18 12:02:03 -0300

    criando as primeiras paginas e rotas (home, login e register)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+/* Pages importeds */
+
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import Home from './components/Home'
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Adote</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/" element={<Home/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
