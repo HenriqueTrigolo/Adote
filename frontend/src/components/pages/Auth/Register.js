48c5895ffb2c3fe2817ad774a2c8cf1feff31432
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2022-10-19 11:29:42 -0300

    cadastrando novo usuario no banco de dados

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,12 +1,16 @@
-import {useState} from 'react'
+import {useContext, useState} from 'react'
 
 import Input from '../../form/Input'
 import styles from '../../form/Form.module.css'
 
 import {Link} from 'react-router-dom'
 
+import {Context} from '../../../context/UserContext'
+
 function Register(){
     const [user, setUser] = useState({})
+    const {register} = useContext(Context)
+
 
     function handleChange(e){
         setUser({...user, [e.target.name]: e.target.value})
@@ -14,6 +18,7 @@ function Register(){
 
     function handleSubmit(e){
         e.preventDefault()
+        register(user)
     }
 
     return(
@@ -24,7 +29,7 @@ function Register(){
                 <Input type="text" text="Telefone" name="phone" placeholder="Digite o seu telefone" handleOnChange={handleChange}/>
                 <Input type="email" text="E-mail" name="email" placeholder="Digite o seu e-mail" handleOnChange={handleChange}/>
                 <Input type="password" text="Senha" name="password" placeholder="Digite a sua senha" handleOnChange={handleChange}/>
-                <Input type="password" text="Confirmação de senha" name="confirmPassword" placeholder="Confirme a sua senha" handleOnChange={handleChange}/>
+                <Input type="password" text="Confirmação de senha" name="confirmpassword" placeholder="Confirme a sua senha" handleOnChange={handleChange}/>
                 <input type="submit" value="Cadastrar"/>
             </form>
             <p>Já tem conta, <Link to="/login">Clique aqui</Link>.</p>
