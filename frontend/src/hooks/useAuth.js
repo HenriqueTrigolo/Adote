7f68aec695336a0274dc6d84a0208dc0c48dfc57
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2022-10-24 11:18:08 -0300

    salvando a autenticação do usuário pós cadastro

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,12 +1,23 @@
 import api from '../utils/api'
 
 import {useState, useEffect} from 'react'
-import{useHistory} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import useFlashMessage from './useFlashMessage'
 
 export default function useAuth(){
 
+    const [authenticated, setAuthenticated] = useState(false)
     const {setFlashMessage} = useFlashMessage()
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+
+        if(token){
+            api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`
+            setAuthenticated(true)
+        }
+    }, [])
 
     async function register(user){
 
@@ -17,6 +28,8 @@ export default function useAuth(){
             const data = await api.post('/users/register', user).then((response)=>{
                 return response.data
             })
+
+            await authUser(data)
         } catch (error) {
             msgText = error.response.data.message
             msgType = 'error'
@@ -25,5 +38,13 @@ export default function useAuth(){
         setFlashMessage(msgText, msgType)
     }
 
-    return {register}
+    async function authUser(data){
+        setAuthenticated(true)
+
+        localStorage.setItem('token', JSON.stringify(data.token))
+
+        navigate('/')
+    }
+
+    return {authenticated, register}
 }
