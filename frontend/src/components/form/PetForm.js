6173810033f56d1c421f46a2eb617a0c0abd54b4
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2022-11-09 12:19:56 -0300

    preview das info dos pets e imagens

--- frontend/src/components/form/PetForm.js
+++ frontend/src/components/form/PetForm.js
@@ -12,16 +12,46 @@ function PetForm({handleSubmit, petData, btnText}){
     const [preview, setPreview] = useState([])
     const colors = ['Branco', 'Preto', 'Caramelo', 'Cinza', 'Mesclado', 'Pintado', 'Outra']
 
-    function onFileChange(e){}
+    function onFileChange(e){
+        setPreview(Array.from(e.target.files))
+        setPet({...pet, images: [...e.target.files]})
+    }
 
-    function handleChange(e){}
+    function handleChange(e){
+        setPet({...pet, [e.target.name]: e.target.value})
+    }
 
     function handleColor(e){
+        setPet({...pet, color: e.target.options[e.target.selectedIndex].text})
+    }
 
+    function submit(e){
+        e.preventDefault()
+        console.log(pet)
+        // handleSubmit(pet)
     }
 
     return(
-        <form className={formStyle.form_container}>
+        <form onSubmit={submit} className={formStyle.form_container}>
+            <div className={formStyle.preview_pet_images}>
+                {preview.length > 0 
+                    ? preview.map((image, index) => (
+                        <img 
+                            src={URL.createObjectURL(image)}
+                            alt={pet.name}
+                            key={`${pet.name}+${index}`}
+                        />
+                    ))
+                    : pet.images &&
+                    pet.images.map((image, index) =>(
+                        <img 
+                            src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                            alt={pet.name}
+                            key={`${pet.name}+${index}`}
+                        />
+                    ))
+                }
+            </div>
             <Input text="Imagens do Pet" type="file" name="images" handleOnChange={onFileChange} multiple={true} />
             <Input text="Nome do Pet" type="text" name="name" placeholder="Digite o nome" handleOnChange={handleChange} value={pet.name || ''} />
             <Input text="Idade do Pet" type="text" name="age" placeholder="Digite a idade" handleOnChange={handleChange} value={pet.age || ''} />
